2d76016eecd75a9e07eb45ba4cc3ab500e05f06b
Sebastian Gonzalez <sebastiangonzalez28@hotmail.com>
2019-04-19 17:26:02 -0500

    Update get.php

--- get.php
+++ get.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-//header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
-header("content-type: application/json");
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
 echo json_encode(array('ok' => 10001));

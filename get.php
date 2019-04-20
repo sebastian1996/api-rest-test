a1c6aee557eb97644f5e28c2cc5857e3123b8702
Sebastian Gonzalez <sebastiangonzalez28@hotmail.com>
2019-04-20 08:25:38 -0500

    Update get.php

--- get.php
+++ get.php
@@ -1,7 +1,6 @@
 <?php
-//header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: *');
 header('content-type: text/javascript');
-//header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
 echo $_GET['jsoncallback'].'('. json_encode(array('ok' => 10001)).');';
 
-?>
